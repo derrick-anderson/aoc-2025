849020173ab4dcd563930bbd42ba383e37bca7b4
Derrick Anderson <derrick.a.cheek@gmail.com>
2025-12-26 02:45:21 +0000

    Add todos and profile

--- day-01/solve.py
+++ day-01/solve.py
@@ -3,8 +3,13 @@ import os
 from typing import List
 
 base_path = os.path.dirname(__file__)
-print(f"Base Path : {base_path}")
 
+def get_instructions() -> List[str]:
+    with open(os.path.join(base_path, "input.txt"), "r") as input:
+        return [line.strip().upper() for line in input.readlines() if line]
+
+#TODO: Add a tracker / statistics to the Safe?
+#TODO: Allow for multiple clicks
 
 class Safe:
     def __init__(self) -> None:
@@ -17,12 +22,11 @@ class Safe:
         return self._dial
 
     def rotate_dial(self, direction: str) -> None:
-        if direction.upper() == "R":
+        if direction == "R":
             self._rotate_right()
-        elif direction.upper() == "L":
+        elif direction == "L":
             self._rotate_left()
         else:
-            print(f"Unsupported Roatation Direction :`{direction}`")
             pass
 
     def _rotate_left(self) -> None:
@@ -32,39 +36,25 @@ class Safe:
 
     def _rotate_right(self) -> None:
         self._dial += 1
-        if self.get_dial() > 99:
+        if self._dial > 99:
             self._dial = self._dial - 100
-
-
-def get_instructions() -> List[str]:
-    instructions = list()
-    with open(os.path.join(base_path, "input.txt"), "r") as input:
-        instructions = [line.strip() for line in input.readlines() if line]
-
-    print(f"Returning {len(instructions)} instructions.")
-    return instructions
 
 
 if __name__ == "__main__":
     safe = Safe()
     safe.set_dial(50)
-    print(f"The Dial starts at {safe.get_dial()}")
     password_count = 0
 
     for instruction in get_instructions():
-        if len(instruction.strip()) == 0:
-            continue
-
-        stripped_instruction = instruction.strip()
-        direction = stripped_instruction[0:1]
-        count = int(stripped_instruction[1:])
+        direction = instruction[0:1]
+        count = int(instruction[1:])
 
         for click in range(count):
             safe.rotate_dial(direction=direction)
             if safe.get_dial() == 0:
                 password_count += 1
-        print(
-            f"The Dial is rotated {stripped_instruction} to point at {safe.get_dial()}"
-        )
+        # print(
+        #     f"The Dial is rotated {instruction} to point at {safe.get_dial()}"
+        # )
 
     print(f'The password is "{password_count}"')
